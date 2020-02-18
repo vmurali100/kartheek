3f6618af3acdd37bf347a3e7fd7fc43db10bdd5b
vmurali100 <vmurali100@gmail.com>
2020-02-18 06:37:17 +0530

    s

--- CRUD_Application/read.js
+++ CRUD_Application/read.js
@@ -42,7 +42,7 @@ function getData() {
     };
     getInfo.open(
       "GET",
-      "http://www.filltext.com/?rows=10&fname={firstName}&lname={lastName}&tel={phone|format}&address={streetAddress}&city={city}&state={usState|abbr}&zip={zip}&pretty=true"
+      "http://www.filltext.com/?rows=3&fname={firstName}&lname={lastName}&tel={phone|format}&address={streetAddress}&city={city}&state={usState|abbr}&zip={zip}&pretty=true"
     );
     getInfo.send();
   });
@@ -50,7 +50,7 @@ function getData() {
 
 async function getInfoFromServer() {
   var users = await getData();
-  displayUses(users);
+  displayUsers(users);
 }
 
 // function getInfoFromServer() {
@@ -60,22 +60,87 @@ async function getInfoFromServer() {
 
 getInfoFromServer();
 
-function displayUses(data) {
-  data.map(function(obj) {
+function displayUsers(data) {
+  document.getElementById("myTable").innerHTML = "";
+  data.map(function(obj, index) {
+    var myTr = document.createElement("tr");
+    //To iterate objects
+    for (a in obj) {
+      var myTd = document.createElement("td");
+      myTd.innerHTML = obj[a];
+      myTr.appendChild(myTd);
+    }
+
+    var editTd = document.createElement("td");
+    var deleteTd = document.createElement("td");
+
+    var editBtn = document.createElement("button");
+    editBtn.innerHTML = "Edit";
+    editBtn.setAttribute("class", "btn btn-warning");
+    editBtn.setAttribute("onclick", "editUser(" + index + ")");
+    var deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "btn btn-danger");
+    deleteBtn.setAttribute("onclick", "delteUser(" + index + ")");
+    deleteBtn.innerHTML = "Delete";
+    editTd.appendChild(editBtn);
+    deleteTd.appendChild(deleteBtn);
+    myTr.appendChild(editTd);
+    myTr.appendChild(deleteTd);
+    document.getElementById("myTable").appendChild(myTr);
     //   Creating Option Element Dynamically
-    var opt = document.createElement("option");
-
+    // var opt = document.createElement("option");
     // Adding Fname to the dynamic Option tag
-    opt.innerHTML = obj.fname;
-
-    document.getElementById("mySelect").appendChild(opt);
+    // opt.innerHTML = obj.fname;
+    // document.getElementById("mySelect").appendChild(opt);
   });
 }
 
+function editUser(index) {
+  gindex = index;
+  console.log(data[index]);
+  var obj = data[index];
+  for (a in obj) {
+    document.getElementById(a).value = obj[a];
+  }
+}
+
+function updateUser() {
+  // Will be used to Clone an Object
+  var updateObj = Object.assign({}, data[0]);
+
+  for (a in updateObj) {
+    updateObj[a] = document.getElementById(a).value;
+  }
+  console.log(updateObj);
+  data[gindex] = updateObj;
+  displayUsers(data);
+  clearForm(updateObj);
+}
+
+function delteUser(index) {
+  console.log(data[index]);
+  data.splice(index, 1);
+  displayUsers(data);
+}
 function getUser() {
   var userFname = document.getElementById("mySelect").value;
   var selectedUser = data.filter(function(obj) {
     return obj.fname == userFname;
   });
   console.log(selectedUser);
+}
+
+function createUser() {
+  var newUser = Object.assign({}, data[0]);
+  for (a in newUser) {
+    newUser[a] = document.getElementById(a).value;
+  }
+  data.push(newUser);
+  displayUsers(data);
+  clearForm(newUser);
+}
+function clearForm(obj) {
+  for (a in obj) {
+    document.getElementById(a).value = "";
+  }
 }
